6d63bbad0496a40295a6f46432b12be7978e5f11
Chris Gill <chris@chrisgill.net>
2022-12-11 05:56:39 -0500

    Setting up to make model downloadable.

--- hubconf.py
+++ hubconf.py
@@ -1,45 +1,81 @@
-####
-# Delivery Vehicles - Hubconf)
-####
+"""PyTorch Hub models
 
-# Model to identify delivery vehicles!
+Usage:
+	import torch
+	model = torch.hub.load('repo','model')
+"""
 
-dependencies = ['torch']
+# Structure learned heavily akhaliq's yolov7 hubconf. Thanks!
+
+from pathlib import Path
 
 import torch
-import pickle
 
-def delivery_vehicles(verbose=True,device=None):
-	""" Creates a Delivery Vehicle model
-	This is a YOLOv5 based model to detect various types of delivery vehicles - UPS, US Postal Service, etc
-	"""
+from models.yolo import Model
+from utils.general import check_requirements, set_logging
+from utils.google_utils import attempt_download
+from utils.torch_utils import select_device
 
-	from utils.general import intersect_dicts, set_logging
-	from utils.torch_utils import select_device
+dependencies = ['torch', 'yaml']
+check_requirements(Path(__file__).parent / 'requirements.txt', exclude=('pycocotools', 'thop'))
+set_logging()
 
-	# 3 channel, RGB images
-	channels = 3
-	# Classes for the model. Currently 4 - Amazon, Fedex, UPS and USPS
-	classes = 4
 
-	# Try to create the model
-	try:
-		# Do we use a CUDA device?
-		device = select_device(('0' if torch.cuda.is_available() else 'cpu') if device is None else device)
-		# Set up the base yolov5 model
-		model = torch.hub.load('WongKinYiu/yolov7','yolov7',classes=classes,device=device)
-		# Fetch the custom weights
-		checkpoint_url = "https://www.jumpbeacon.net/dv_model/dv_latest.pt"
-		ckpt = torch.hub.load_state_dict_from_url(checkpoint_url,map_location=device)
-		# Checkpoint state_dict as FP32
-		csd = ckpt['model'].float().state_dict()
-		# Merge the new weights with the existing model state_dict
-		csd = intersect_dicts(csd, model.state_dict(), exclude=['anchors'])
-		# Load in the merged state
-		model.load_state_dict(csd, strict=False)
+def create(name, pretrained, channels, classes, autoshape):
+    """Creates a specified model
+    Arguments:
+        name (str): name of model, i.e. 'yolov7'
+        pretrained (bool): load pretrained weights into the model
+        channels (int): number of input channels
+        classes (int): number of model classes
+    Returns:
+        pytorch model
+    """
+    try:
+	cfg = list((Path(__file__).parent / 'cfg').rglob(f'{name}.yaml'))[0]  # model.yaml path
+	model = Model(cfg, channels, classes)
+	if pretrained:
+		fname = f'{name}.pt'  # checkpoint filename
+		attempt_download(fname, repo="chrisgilldc/delv")  # download if not found locally
+		ckpt = torch.load(fname, map_location=torch.device('cpu'))  # load
+		msd = model.state_dict()  # model state_dict
+		csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
+		csd = {k: v for k, v in csd.items() if msd[k].shape == v.shape}  # filter
+		model.load_state_dict(csd, strict=False)  # load
 		if len(ckpt['model'].names) == classes:
-			model.names = ckpt['model'].names # Set the class names attribute
-		return model.to(device)
+			model.names = ckpt['model'].names  # set class names attribute
+		if autoshape:
+			model = model.autoshape()  # for file/URI/PIL/cv2/np inputs and NMS
+
+	device = select_device('0' if torch.cuda.is_available() else 'cpu')  # default to GPU if available
+	return model.to(device)
 
 	except Exception as e:
-		raise e
+		s = 'Cache maybe be out of date, try force_reload=True.'
+		raise Exception(s) from e
+
+
+def custom(path_or_model='path/to/model.pt', autoshape=True):
+	"""custom mode
+	Arguments (3 options):
+		path_or_model (str): 'path/to/model.pt'
+		path_or_model (dict): torch.load('path/to/model.pt')
+		path_or_model (nn.Module): torch.load('path/to/model.pt')['model']
+
+	Returns:
+		pytorch model
+	"""
+	model = torch.load(path_or_model) if isinstance(path_or_model, str) else path_or_model  # load checkpoint
+	if isinstance(model, dict):
+		model = model['ema' if model.get('ema') else 'model']  # load model
+
+	hub_model = Model(model.yaml).to(next(model.parameters()).device)  # create
+	hub_model.load_state_dict(model.float().state_dict())  # load state_dict
+	hub_model.names = model.names  # class names
+	if autoshape:
+		hub_model = hub_model.autoshape()  # for file/URI/PIL/cv2/np inputs and NMS
+	device = select_device('0' if torch.cuda.is_available() else 'cpu')  # default to GPU if available
+	return hub_model.to(device)
+
+def delv(pretrained=True, channels=3, classes=4, autoshape=True):
+    return create('dv2aug2', pretrained, channels, classes, autoshape)

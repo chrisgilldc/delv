9910acc1b976b4332898c61cdf6d5e9fd4a5e372
Chris <chris@chrisgill.net>
2023-03-06 14:42:26 -0500

    Create separate entry points.

--- hubconf.py
+++ hubconf.py
@@ -10,12 +10,31 @@ from pathlib import Path
 
 dependencies = ['torch', 'yaml']
 
-def delv(pretrained=True, channels=3, classes=4, autoshape=True):
-        # Download the model file.
-        file = Path("dv2aug2.pt")
-        if not file.exists():
-                url = f"https://github.com/chrisgilldc/delv/releases/download/0.1-alpha/{file}"
-                torch.hub.download_url_to_file(url, file)
-        # Create Yolov7 with the custom model.
-        model = torch.hub.load('WongKinYiu/yolov7','custom',path_or_model='./dv2aug2.pt')
-        return model
+def _delv(model, pretrained=True, channels=3, classes=4, autoshape=True):
+    valid_models = ('dv1-100', 'dv1-200', 'dv1a1-100', 'dv1a1-200', 'dv1a1-500')
+    if model not in valid_models:
+        raise ValueError("{} is not a valid model name.".format(model))
+    # Build the file name.
+    file = Path(model).with_suffix('pt')
+    # If the file doesn't exist, download it.
+    if not file.exists():
+        url = f"https://github.com/chrisgilldc/delv/releases/download/0.2-alpha/{file}"
+        torch.hub.download_url_to_file(url, file)
+    # Load the mode with those
+    model = torch.hub.load('WongKinYiu/yolov7', 'custom', path_or_model=str(file))
+    return model
+
+def dv1_100(pretrained=True, channels=3, classes=4, autoshape=True):
+    return _delv('dv1_100', pretrained, channels, classes, autoshape)
+
+def dv1_200(pretrained=True, channels=3, classes=4, autoshape=True):
+    return _delv('dv1_200', pretrained, channels, classes, autoshape)
+
+def dv1a1_100(pretrained=True, channels=3, classes=4, autoshape=True):
+    return _delv('dv1a1_100', pretrained, channels, classes, autoshape)
+
+def dv1a1_200(pretrained=True, channels=3, classes=4, autoshape=True):
+    return _delv('dv1a1_200', pretrained, channels, classes, autoshape)
+
+def dv1a1_500(pretrained=True, channels=3, classes=4, autoshape=True):
+    return _delv('dv1a1_500', pretrained, channels, classes, autoshape)

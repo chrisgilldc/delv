68ff45731c23d4ffd6e5b24424c12358f5de5f49
Chris <chris@chrisgill.net>
2023-03-31 14:38:37 -0400

    Updated to work around issues with Yolov7's custom model loader. Loads the yolo classes itself and then unpickles the models with torch.load.

--- hubconf.py
+++ hubconf.py
@@ -7,22 +7,35 @@ Usage:
 
 import torch
 from pathlib import Path
+import zipfile
+import sys
 
 dependencies = ['torch', 'yaml']
 
 def _delv(model, pretrained=True, channels=3, classes=4, autoshape=True):
-    valid_models = ('dv1-100', 'dv1-200', 'dv1a1-100', 'dv1a1-200', 'dv1a1-500')
+    valid_models = ('dv1_100', 'dv1_200', 'dv1a1_100', 'dv1a1_200', 'dv1a1_500')
     if model not in valid_models:
         raise ValueError("{} is not a valid model name.".format(model))
     # Build the file name.
-    file = Path(model).with_suffix('pt')
+    dv_file = Path(model).with_suffix('.pt')
     # If the file doesn't exist, download it.
-    if not file.exists():
-        url = f"https://github.com/chrisgilldc/delv/releases/download/0.2-alpha/{file}"
-        torch.hub.download_url_to_file(url, file)
-    # Load the mode with those
-    model = torch.hub.load('WongKinYiu/yolov7', 'custom', path_or_model=str(file))
-    return model
+    if not dv_file.exists():
+        dv_url = f"https://github.com/chrisgilldc/delv/releases/download/0.3-alpha/{dv_file}"
+        torch.hub.download_url_to_file(dv_url, dv_file)
+    # Download the yolov7 archive.
+    yolov7_url = "https://github.com/WongKinYiu/yolov7/archive/refs/heads/main.zip"
+    torch.hub.download_url_to_file(yolov7_url, "yolov7.zip")
+    zipfile.ZipFile("yolov7.zip").extractall()
+    # Extend the system search path.
+    sys.path.append(str(Path.cwd() / "yolov7-main"))
+    print("New system path: {}".format(sys.path))
+    # Now we can import the Yolo model.
+    from models.yolo import Model
+
+    # Load the pickled model
+    dv_model = torch.load(dv_file, map_location=torch.device('cpu')).model
+    return dv_model
+
 
 def dv1_100(pretrained=True, channels=3, classes=4, autoshape=True):
     return _delv('dv1_100', pretrained, channels, classes, autoshape)

f9f81897767127525d3e98a2423097845df23085
Chris <chris@chrisgill.net>
2023-04-01 19:15:13 -0400

    Unpickeled model doesn't work, may need to put this through YoloV7s' actual custom loader to get it sorted properly.

--- hubconf.py
+++ hubconf.py
@@ -14,6 +14,7 @@ dependencies = ['torch', 'yaml']
 
 def _delv(model, pretrained=True, channels=3, classes=4, autoshape=True):
     valid_models = ('dv1_100', 'dv1_200', 'dv1a1_100', 'dv1a1_200', 'dv1a1_500')
+
     if model not in valid_models:
         raise ValueError("{} is not a valid model name.".format(model))
     # Build the file name.
@@ -24,17 +25,22 @@ def _delv(model, pretrained=True, channels=3, classes=4, autoshape=True):
         dv_url = f"https://github.com/chrisgilldc/delv/releases/download/0.3-alpha/{dv_file}"
         torch.hub.download_url_to_file(dv_url, dv_file)
     # Download the yolov7 archive.
-    yolov7_url = "https://github.com/WongKinYiu/yolov7/archive/refs/heads/main.zip"
-    torch.hub.download_url_to_file(yolov7_url, "yolov7.zip")
-    zipfile.ZipFile("yolov7.zip").extractall()
-    # Extend the system search path.
-    sys.path.append(str(Path.cwd() / "yolov7-main"))
+    yolov7_dir = Path.cwd() / 'yolov7-main'
+    if not yolov7_dir.exists():
+        yolov7_url = "https://github.com/WongKinYiu/yolov7/archive/refs/heads/main.zip"
+        torch.hub.download_url_to_file(yolov7_url, "yolov7.zip")
+        zipfile.ZipFile("yolov7.zip").extractall()
+        # Clean up the archive.
+        os.remote("yolov7.zip")
+    # Extend the system path so imports will work properly.
+    sys.path.append(str(yolov7_dir))
     # Now we can import the Yolo model.
     from models.yolo import Model
-
     # Load the pickled model
     dv_model = torch.load(dv_file, map_location=torch.device('cpu')).model
-    return dv_model
+    # Now we can instantiate the full model.
+    model = torch.hub.load("WongKinYiu/yolov7","custom",path_or_model=dv_model)
+    return model
 
 
 def dv1_100(pretrained=True, channels=3, classes=4, autoshape=True):
